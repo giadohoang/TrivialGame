a1f97a0fd24f03a6e284f6b50d62931a480f6474
Gia Do <gia.dohoang@gmail.com>
2020-04-12 22:07:06 -0400

    Initial Commit with Identity Fremwork

---
+++ TrivialGame/TrivialGame/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TrivialGame.Data;
+
+[assembly: HostingStartup(typeof(TrivialGame.Areas.Identity.IdentityHostingStartup))]
+namespace TrivialGame.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
